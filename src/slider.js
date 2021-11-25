763552fbe74a60073b6827c72618fc10e58358c0
Milos Danilovic <danilovicmilos25@gmail.com>
2021-11-25 19:29:18 +0100

    Added the modal window anc changed the layout around

--- src/slider.js
+++ src/slider.js
@@ -14,10 +14,11 @@ fetch(url)
   });
 
 let slidePosition = 0;
+const carouselContainer = document.getElementById("carousel");
 const slides = document.getElementsByClassName("carousel__item");
 const slideElements = document.getElementsByClassName("carousel__element");
 const totalSlides = slides.length;
-
+slideTimer = setInterval(moveToNextSlide, 3000);
 document
   .getElementById("carousel__button--next")
   .addEventListener("click", function () {
@@ -47,6 +48,7 @@ function moveToNextSlide() {
 
   updateSlidePosition();
 }
+//turn slide very 3 seconds
 
 function moveToPrevSlide() {
   if (slidePosition === 0) {
@@ -57,7 +59,29 @@ function moveToPrevSlide() {
 
   updateSlidePosition();
 }
+function allowArrows(e) {
+  switch (e.key) {
+    case "Left": // IE/Edge specific value
+    case "ArrowLeft":
+      moveToPrevSlide();
 
+      break;
+
+    case "Right": // IE/Edge specific value
+    case "ArrowRight":
+      moveToNextSlide();
+
+      break;
+  }
+}
+function inContainer() {
+  document.addEventListener("keydown", allowArrows);
+}
+function removeEvent() {
+  document.removeEventListener("keydown", allowArrows);
+}
+carouselContainer.addEventListener("mouseenter", inContainer);
+carouselContainer.addEventListener("mouseleave", removeEvent);
 function addImages() {
   // var readMore = document.createElement("a");
   // readMore.classList.add("myButton");
@@ -66,8 +90,11 @@ function addImages() {
   let counter = 0;
   for (let item of slideElements) {
     var readMore = document.createElement("a");
+
     readMore.classList.add("myButton");
+    readMore.setAttribute("id", "readMore");
     readMore.innerText = "Read More";
+
     var image = document.createElement("IMG");
     var title = document.createElement("p");
     title.classList.add("carousel__item_text");
@@ -79,6 +106,29 @@ function addImages() {
     title.append(readMore);
     console.log(title);
 
+    //modal
+    readMore.onclick = function () {
+      modal.style.display = "block";
+    };
+
     counter++;
   }
 }
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("readMore");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
