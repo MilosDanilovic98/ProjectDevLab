f36285dee0d0911fd0ba5cb25b6e4681ddccfed2
Aleksandar Drašković <aco.draskovic2016@gmail.com>
2021-11-28 13:39:37 +0100

    Bugfix. Added api data to topnav breaking news

--- header/header.js
+++ header/header.js
@@ -1,3 +1,34 @@
+
+const proxyUrl = "https://cors-anywhere.herokuapp.com/"
+const instance = axios.create({
+  baseURL: `${proxyUrl}https://newsapi.org/v2/`,
+  timeout: 1000,
+  headers: {
+     'Authorization': "b562823db8174e7f8b2e02152da43a83",
+    'access-control-allow-origin': '*'}
+});
+
+
+
+instance.get('top-headlines', {
+  params: {
+    country: 'us',
+    pageSize: 3
+  }
+}).then(res => {
+  const topNews = document.getElementById('topnews');
+  res.data.articles.forEach(item => {
+    var tag = document.createElement("a"); // <p></p>
+    let date = new Date(item.publishedAt)
+  var text = document.createTextNode(` (${date.toLocaleDateString("en-US")}) ${item.title}`); 
+  tag.appendChild(text); // <p>TEST TEXT</p>
+  console.log(text);
+  
+    console.log(item);
+    topNews.appendChild(tag);
+  })
+  // console.log(res.data);
+})
 //TOP NAVIGATION
 function NavBar() {
   var x = document.getElementById("myTopnav");
