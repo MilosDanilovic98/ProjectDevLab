d2e93cd7e4d0da7185335b2a5984690063f86695
Aleksandar Drašković <aco.draskovic2016@gmail.com>
2021-12-02 15:40:50 +0100

    Fixed about us

--- app/about-us.js
+++ app/about-us.js
@@ -5,22 +5,22 @@ var control3 = document.getElementById('control3');
 
 control1.onclick = function()
 {
-    testimonials.style.transform = "translate(870px)";
+    testimonials.style.setProperty("transform", "translate(870px)", "important");
     control1.classList.add("active")
-    control1.classList.remove("active")
-    control1.classList.remove("active")
+    control2.classList.remove("active")
+    control3.classList.remove("active")
 }
 control2.onclick = function()
 {
-    testimonials.style.transform = "translate(0px)";
-    control1.classList.remove("active")
-    control1.classList.add("active")
-    control1.classList.remove("active")
+    testimonials.style.setProperty("transform", "translate(0px)", "important");
+       control1.classList.remove("active")
+    control2.classList.add("active")
+    control3.classList.remove("active")
 }
 control3.onclick = function()
 {
-    testimonials.style.transform = "translate(-870px)";
+    testimonials.style.setProperty("transform", "translate(-870px)", "important");
     control1.classList.remove("active")
-    control1.classList.remove("active")
-    control1.classList.add("active")
+    control2.classList.remove("active")
+    control3.classList.add("active")
 }
